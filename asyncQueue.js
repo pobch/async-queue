bffda142504f6b98529ea5a1ecdb9e6c0b74031f
Pob <590650@gmail.com>
2019-10-26 14:21:30 +0700

    first test and fail

--- asyncQueue.js
+++ asyncQueue.js
@@ -1,1 +1,21 @@
-function createQueue(tasks, numOfWorkers = 3) {}
+import { createReq } from './mock'
+
+function createQueue(tasks, maxNumOfWorkers = 3) {
+  let taskIndex = 0
+  let numOfWorker = 0
+  // TODO: create closure
+}
+
+// Usage
+createQueue([
+  createReq(2000),
+  createReq(500),
+  createReq(1000),
+  createReq(3000),
+  createReq(50),
+  createReq(1300),
+  createReq(800),
+  createReq(2700)
+]).then(results => {
+  console.log(results)
+})
